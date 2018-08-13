001d2742b95c9b7d690f81bb6d65d3d024fbbe80
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-13 02:37:45 +0200

    init back nodejs rest api server

---
+++ back/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const util = require('util');
+
+// config should be imported before importing any other file
+const config = require('./config/config');
+const app = require('./config/express');
+
+const debug = require('debug')('express-mongoose-es6-rest-api:index');
+
+// make bluebird default Promise
+Promise = require('bluebird'); // eslint-disable-line no-global-assign
+
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
+
+// connect to mongo db
+const mongoUri = config.mongo.host;
+mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+// print mongoose logs in dev env
+if (config.mongooseDebug) {
+  mongoose.set('debug', (collectionName, method, query, doc) => {
+    debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+  });
+}
+
+// module.parent check is required to support mocha watch
+// src: https://github.com/mochajs/mocha/issues/1912
+if (!module.parent) {
+  // listen on port config.port
+  app.listen(config.port, () => {
+    console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+  });
+}
+
+module.exports = app;
