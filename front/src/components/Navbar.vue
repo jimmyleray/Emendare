09d47a8ca438162e5b0288a200286bf383edd7dc
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-14 00:07:29 +0200

    add navbar component

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-toolbar app dark color="primary">
+        <v-toolbar-title>Emendare</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon to="/">
+            <v-icon>home</v-icon>
+        </v-btn>
+        <v-btn icon to="/groups">
+            <v-icon>supervised_user_circle</v-icon>
+        </v-btn>
+        <v-btn icon to="/about">
+            <v-icon>help</v-icon>
+        </v-btn>
+    </v-toolbar>
+</template>
