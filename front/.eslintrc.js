38f75e8a0c71b889c84d0b81cf47768ce868f4a2
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-13 02:13:01 +0200

    init front app with vue cli 3

---
+++ front/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/prettier'
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
---
+++ front/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    name: "Emendare"
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined,
+  lintOnSave: undefined
+};
