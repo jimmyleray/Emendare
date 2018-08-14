60740bd898fa7437ecadf0479d6150bca2b08c74
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-14 14:00:46 +0200

    pretttier config set to default

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it("should create the app", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -14,14 +14,14 @@ describe('AppComponent', () => {
   it(`should have as title 'front'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('front');
+    expect(app.title).toEqual("front");
   }));
-  it('should render title in a h1 tag', async(() => {
+  it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to front!'
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to front!"
     );
   }));
 });
