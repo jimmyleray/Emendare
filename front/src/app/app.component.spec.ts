1fa4a7d9586fc4e11b70a87101521235c1c2e209
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-15 16:39:22 +0200

    add custom_schema on tests

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
