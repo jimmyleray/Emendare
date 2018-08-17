705b462a7365b2c9d3f63f479328df7d15a0ca78
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-17 14:40:17 +0200

    add readme path into app config

--- front/src/app/config/app.config.ts
+++ front/src/app/config/app.config.ts
@@ -1,1 +1,2 @@
 export const appTitle = "Emendare";
+export const appReadme = "https://raw.githubusercontent.com/JimmyLeray/Emendare/master/README.md"
--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { appReadme } from "src/app/config";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public appReadme = appReadme;
+}
