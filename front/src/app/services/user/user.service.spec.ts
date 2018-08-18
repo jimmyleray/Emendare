5892eed3543965daee6ab51b9193547d6ff30260
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-18 20:03:06 +0200

    add providers to correct a test

--- front/src/app/services/user/user.service.spec.ts
+++ front/src/app/services/user/user.service.spec.ts
@@ -4,7 +4,9 @@ import { UserService } from "./user.service";
 
 describe("UserService", () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [UserService]
+    });
   });
 
   it("should be created", inject([UserService], (service: UserService) => {
