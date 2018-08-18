2b8818f2964faa3659f7ab686fe0901d85e839f7
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-18 16:20:06 +0200

    app scroll bar fix at right

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component } from "@angular/core";
-import { appConfig } from "src/app/config";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
-export class HomeComponent {
-  public appConfig = appConfig;
-}
+export class HomeComponent {}
