dce6af4e4886223e4ce68a01421a358f6ee31d04
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-08-19 20:05:55 +0200

    sign-out redirect to home page

--- front/src/app/pages/users/sign-out/sign-out.component.ts
+++ front/src/app/pages/users/sign-out/sign-out.component.ts
@@ -1,7 +1,9 @@
+import { appRoutes } from "src/app/config";
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { SignOutAction } from "src/app/store/actions";
 import { AppState } from "src/app/store/app.state";
+import { Router } from "@angular/router";
 
 /**
  * Sign-out page component
@@ -11,16 +13,19 @@ import { AppState } from "src/app/store/app.state";
   styleUrls: ["./sign-out.component.scss"]
 })
 export class SignOutComponent implements OnInit {
+  public appRoutes = appRoutes;
+
   /**
    * @constructor Constructor function
    * @param {Store<AppState>} store Ngrx store service
    */
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>, private router: Router) {}
 
   /**
    * @method ngOnInit Lifecycle hook that is called after data-bound properties of a directive are initialized.
    */
   ngOnInit() {
     this.store.dispatch(new SignOutAction());
+    this.router.navigate([appRoutes.HOME]);
   }
 }
