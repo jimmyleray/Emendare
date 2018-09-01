e066d2cf838e2968c09fe15144557a88f8803924
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-09-02 00:05:31 +0200

    update error page

--- elm/src/Pages/NotFound.elm
+++ elm/src/Pages/NotFound.elm
@@ -3,6 +3,8 @@ module Pages.NotFound exposing (view)
 import Html exposing (Html, section, div, h1, h2, text)
 import Html.Attributes exposing (class) 
 
+import Services.Translate.Keys exposing (TranslationKey(..))
+import Services.Translate.Main exposing (translate)
 import Services.Core.Model exposing (Model)
 import Services.Core.Messages exposing (Msg)
 
@@ -10,11 +12,11 @@ import Services.Core.Messages exposing (Msg)
 
 view : Model -> Html Msg
 view model =
-    section [ class "hero is-dark" ]
-            [ div [ class "hero-body" ]
+    section [ class "hero is-warning is-fullheight" ]
+            [ div [ class "hero-body has-text-centered" ]
                 [ div [ class "container" ]
-                    [ h1 [ class "title" ] [ text "Error 404" ]
-                    , h2 [ class "subtitle" ] [ text "Not Found" ]
+                    [ h1 [ class "title" ] [ text <| translate model.language Error404 ]
+                    , h2 [ class "subtitle" ] [ text <| translate model.language NotFound ]
                     ]
                 ]
             ]
