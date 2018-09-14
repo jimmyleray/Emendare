350700cc0020f33137284cf425eaf66fbbe01073
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-09-14 22:26:19 +0200

    start REST API on server

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,47 @@
+// Global configuration
+const config = require("./config");
+
+// Http promise requests
+const axios = require("axios");
+
+// Express Html Application
 const express = require("express");
 const app = express();
 
-// Request list of groups
-const groups = require("./mocks/groups");
+// Requests Logger Middleware
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
+// Get list of all root groups
 app.get("/groups", (req, res) => {
-  res.json(groups);
+  const url = config.gitlabAPIUrl + `/groups/${config.rootGroupName}/subgroups`;
+  axios.get(url).then(({ data }) => {
+    res.json(data);
+  });
 });
 
-// Request list of grotextsups
-const texts = require("./mocks/texts");
-app.get("/texts", (req, res) => {
-  res.json(texts);
+// Get a specific group
+app.get("/groups/:id", (req, res) => {
+  const url = config.gitlabAPIUrl + `/groups/${req.params.id}`;
+  axios.get(url).then(({ data }) => {
+    res.json(data);
+  });
 });
 
-// Error 404 middleware
+// Get list of all projects in a specific group
+app.get("/groups/:id/projects", (req, res) => {
+  const url = config.gitlabAPIUrl + `/groups/${req.params.id}/projects`;
+  axios.get(url).then(({ data }) => {
+    res.json(data);
+  });
+});
+
+// Error 404 Middleware
 app.use((req, res) => {
   res.status(404).send("Not Found");
 });
 
-// Start the server
-app.listen(8080, () => {
-  console.log("Example app listening on port 8080.");
+// Start Html Server
+app.listen(config.port, () => {
+  console.log(`Server start on port ${config.port}.`);
 });
