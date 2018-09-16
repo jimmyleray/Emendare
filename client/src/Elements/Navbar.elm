23d923531c75a32efe6c713faf0401f20da4f7c6
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-09-16 21:42:19 +0200

    move explorer link at left of navbar

--- client/src/Elements/Navbar.elm
+++ client/src/Elements/Navbar.elm
@@ -1,6 +1,6 @@
 module Elements.Navbar exposing (view)
 
-import Html exposing (Html, nav, div, a, span, i, text)
+import Html exposing (Html, nav, div, a, span, i, text, strong)
 import Html.Attributes exposing (attribute, class, href, title, target)
 import Html.Events exposing (onClick)
 
@@ -19,11 +19,12 @@ view model =
     nav [ attribute "aria-label" "main navigation", class "navbar is-dark is-fixed-top", attribute "role" "navigation" ]
         [ div [ class "container" ]
             [ div [ class "navbar-brand" ]
-                [ a [ class "navbar-item", href <| getRouteUrl Root ] [ text "Emendare" ] ]
+                [ a [ class "navbar-item", href <| getRouteUrl Root ] [ text "Emendare" ]
+                , Elements.Link.view model Explore
+                ]
             , div [ class "navbar-menu" ]
                 [ div [ class "navbar-end" ] 
-                    [ Elements.Link.view model Explore
-                    , Elements.Link.view model (if model.isAuthentified then Profile else SignIn)
+                    [ Elements.Link.view model (if model.isAuthentified then Profile else SignIn)
                     , a [ class "navbar-item", href "https://github.com/jimmyleray/Emendare", title "GitHub", target "_blank" ]
                         [ span [ class "icon fa-lg" ]
                             [ i [ class "fab fa-github" ] [] ]
