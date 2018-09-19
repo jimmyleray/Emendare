39e58178a95bc720bbbb5dc83a2f122581acb0d0
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-09-19 16:33:48 +0200

    add breadcrumb in text page

--- client/src/Pages/Text.elm
+++ client/src/Pages/Text.elm
@@ -1,20 +1,21 @@
 module Pages.Text exposing (view)
 
 import Markdown
-import Html exposing (Html, section, div, h1, h2, text)
-import Html.Attributes exposing (class) 
+import Html exposing (..)
+import Html.Attributes exposing (class, href) 
 
 import Services.Translate.Keys exposing (TranslationKey(..))
 import Services.Translate.Main exposing (translate)
 import Services.Core.Model exposing (Model, Text)
 import Services.Core.Messages exposing (Msg)
+import Services.Routing.Routes exposing (Route(..), getRouteUrl)
 
 
 
 view : Model -> Int -> Html Msg
 view model id =
     section [ class "hero is-fullheight" ]
-            [ div [ class "hero-body has-text-centered" ]
+            [ div [ class "hero-body" ]
                 [ case model.text of
                     Just txt ->
                         viewContainer model txt
@@ -28,12 +29,21 @@ view model id =
 
 viewContainer : Model -> Text -> Html Msg
 viewContainer model txt =
-    div [ class "container" ]
-        [ h1 [ class "title" ] [ text <| (translate model.language TextTitle) ++ " : " ++ txt.name ] 
+    div [ class "markdown-container" ]
+        [ nav [ class "breadcrumb" ]
+            [ ul []
+                [ li []
+                    [ a [ href <| getRouteUrl <| Group txt.namespace.id ]
+                        [ text txt.namespace.name ]
+                    ]
+                , li [ class "is-active" ] [ a [ href "" ] [ text txt.name ] ]
+                ]
+            ]
+        , h1 [ class "title" ] [ text txt.name ] 
         , h2 [ class "subtitle" ] [ text txt.description ]
         , case txt.content of
             Just content ->
-                Markdown.toHtml [class "box markdown-body has-text-left"] content
+                Markdown.toHtml [class "box markdown-body"] content
                 
             Nothing ->
                 div [] []
