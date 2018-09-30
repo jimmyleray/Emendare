a49e438f3bd17c8d67a8a45fe3a0de21e63afc20
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-09-30 13:23:05 +0200

    add a home link on NotFound page

--- front/src/views/NotFound.vue
+++ front/src/views/NotFound.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-layout align-center justify-center>
+  <v-layout column align-center justify-center>
     <h1>Oups cette page n'existe pas ou plus</h1>
+    <v-btn to="/" exact color="success">Retour à l'accueil</v-btn>
   </v-layout>
 </template>
--- front/src/views/Pending.vue
+++ front/src/views/Pending.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout align-center justify-center>
+  <v-layout column align-center justify-center>
     <v-progress-circular indeterminate color="primary" :size="128" :width="8">
       Chargement
     </v-progress-circular>
