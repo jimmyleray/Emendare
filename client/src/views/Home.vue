137ea176f2a2a1646a655645fb56ece0a1e1c74f
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-11-02 22:01:16 +0100

    add bold style on home alert

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,7 +4,7 @@
       <h1>Plateforme open source de rédaction de textes amendables</h1>
       <h2>Un amendement est une modification d'un texte, soumise au vote d'un groupe</h2>
       <v-alert :value="true" type="info">
-        Version <strong>alpha privée</strong> ouverte depuis le 16 Novembre 2018 uniquement pour les mails *@zenika.com<br>
+        <strong>Version alpha</strong> ouverte depuis le 16 Novembre 2018 uniquement pour les mails *@zenika.com<br>
         Pour participer au développement vous pouvez rejoindre le channel Slack <strong>#emendare</strong>
       </v-alert>
       <v-btn block to="/signup" color="success">Créer votre compte</v-btn>
