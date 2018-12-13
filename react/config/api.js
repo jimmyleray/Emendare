3a79754fb14f8b46317e943d0554724ed703ea6a
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-12-13 19:16:30 +0100

    update api service

--- react/config/api.js
+++
@@ -1,6 +0,0 @@
-export default {
-  url: {
-    development: 'http://localhost:3000',
-    production: 'https://emendare-api.cleverapps.io'
-  }
-}
--- react/services/api.js
+++ react/services/api.js
@@ -1,4 +1,11 @@
-import config from '../config/api'
-import { environment } from './env'
-
-export const api = pathname => config.url[environment] + pathname
+const config = {
+  url: {
+    development: 'http://localhost:3000',
+    production: 'https://emendare-api.cleverapps.io'
+  }
+}
+export class Api {
+  static call(pathname) {
+    return config.url[process.env.NODE_ENV] + pathname
+  }
+}
