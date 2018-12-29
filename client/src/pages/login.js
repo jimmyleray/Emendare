6ff0e4a870d342c7f421109a992ce09cefeae025
Jimmy LERAY <jimmy.leray@protonmail.com>
2018-12-29 16:48:28 +0100

    update login page todos

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,7 +2,7 @@
  * Page de connexion
  * Le but de cette page est de permettre aux utilisateurs :
  * - de se connecter avec leur email / mot de passe
- * - TODO : d'accèder à la page d'inscription
+ * - d'accèder à la page d'inscription
  * - TODO : d'accèder à une page de récupération de mot de passe
  */
 
