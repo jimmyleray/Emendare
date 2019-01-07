96a55bf406ceeed7323de73c2f2002b1509668a9
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-07 12:30:22 +0100

    fix a bug with disabled button link

--- client/src/components/0_atoms/button.js
+++ client/src/components/0_atoms/button.js
@@ -3,9 +3,17 @@ import { Link } from 'react-router-dom'
 
 export const Button = ({ children, className = '', ...rest }) =>
   rest.to ? (
-    <Link to={rest.to} className={'button ' + className} {...rest}>
-      {children}
-    </Link>
+    <>
+      {rest.disabled ? (
+        <button className={'button ' + className} {...rest}>
+          {children}
+        </button>
+      ) : (
+        <Link to={rest.to} className={'button ' + className} {...rest}>
+          {children}
+        </Link>
+      )}
+    </>
   ) : (
     <button className={'button ' + className} {...rest}>
       {children}
