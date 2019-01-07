bb20f2cc3c36aeddaf9d6aebc4f8fe3edfa77530
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-07 18:40:59 +0100

    fix max amend delay to 1 day

--- server/src/models/amend.js
+++ server/src/models/amend.js
@@ -20,7 +20,7 @@ const model = mongoose.model(
     upVotesCount: { type: Number, default: 0 },
     downVotesCount: { type: Number, default: 0 },
     delayMin: { type: Number, default: 60 * 60 * 1000 },
-    delayMax: { type: Number, default: 7 * 24 * 60 * 60 * 1000 },
+    delayMax: { type: Number, default: 24 * 60 * 60 * 1000 },
     closed: { type: Boolean, default: false },
     accepted: { type: Boolean, default: false },
     conflicted: { type: Boolean, default: false }
