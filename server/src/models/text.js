e580b05bd9c5d47452f632c21a2581ecb9d4f8c8
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-01-08 15:02:35 +0100

    add lint and prettier for the backend

--- server/src/models/text.js
+++ server/src/models/text.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose')
 
 const model = mongoose.model(
-  'Text',
-  new mongoose.Schema({
-    created: { type: Date, default: Date.now },
-    name: { type: String, default: '' },
-    description: { type: String, default: '' },
-    followersCount: { type: Number, default: 0 },
-    actual: { type: String, default: '' },
-    patches: { type: [String], default: [] },
-    group: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Group',
-      required: true
-    },
-    amends: {
-      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Amend' }],
-      default: []
-    },
-    rules: { type: Boolean, default: false }
-  })
+	'Text',
+	new mongoose.Schema({
+		created: { type: Date, default: Date.now },
+		name: { type: String, default: '' },
+		description: { type: String, default: '' },
+		followersCount: { type: Number, default: 0 },
+		actual: { type: String, default: '' },
+		patches: { type: [String], default: [] },
+		group: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Group',
+			required: true
+		},
+		amends: {
+			type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Amend' }],
+			default: []
+		},
+		rules: { type: Boolean, default: false }
+	})
 )
 
 module.exports = class Text {
-  static get model() {
-    return model
-  }
+	static get model() {
+		return model
+	}
 }
--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto')
 
 module.exports = () => {
-  const buffer = crypto.randomBytes(256)
-  return buffer.toString('hex')
+	const buffer = crypto.randomBytes(256)
+	return buffer.toString('hex')
 }
