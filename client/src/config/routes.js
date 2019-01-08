9bf0494b736e2c3ae45263670dccd0f5046fa42b
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-01-08 11:43:55 +0100

    add linter and prettier for the client

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -3,24 +3,24 @@ const withID = path => (id = ':id') => path + id
 
 // All application routes
 export const routes = [
-  { name: 'home', path: '/', exact: true },
-  { name: 'news', path: '/actualites' },
-  { name: 'roadmap', path: '/roadmap' },
-  { name: 'code', path: '/charte-ethique' },
-  { name: 'legal', path: '/mentions-legales' },
-  { name: 'contributors', path: '/contributeurs' },
-  { name: 'explore', path: '/explorer' },
-  { name: 'group', path: withID('/groupe/') },
-  { name: 'login', path: '/connexion' },
-  { name: 'subscribe', path: '/inscription' },
-  { name: 'profile', path: '/profil', private: true },
-  { name: 'text', path: withID('/texte/') },
-  { name: 'edit', path: withID('/editer/'), private: true },
-  { name: 'amend', path: withID('/amendement/') }
+	{ name: 'home', path: '/', exact: true },
+	{ name: 'news', path: '/actualites' },
+	{ name: 'roadmap', path: '/roadmap' },
+	{ name: 'code', path: '/charte-ethique' },
+	{ name: 'legal', path: '/mentions-legales' },
+	{ name: 'contributors', path: '/contributeurs' },
+	{ name: 'explore', path: '/explorer' },
+	{ name: 'group', path: withID('/groupe/') },
+	{ name: 'login', path: '/connexion' },
+	{ name: 'subscribe', path: '/inscription' },
+	{ name: 'profile', path: '/profil', private: true },
+	{ name: 'text', path: withID('/texte/') },
+	{ name: 'edit', path: withID('/editer/'), private: true },
+	{ name: 'amend', path: withID('/amendement/') }
 ]
 
 // To be use like that : path.text("42") => /text/42
 export const path = routes.reduce(
-  (acc, route) => ({ ...acc, [route.name]: route.path }),
-  {}
+	(acc, route) => ({ ...acc, [route.name]: route.path }),
+	{}
 )
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -7,29 +7,29 @@ const insecureSocket = io(apiConfig.url[process.env.NODE_ENV])
 // Overwrited Socket.io Instance
 // With fetch method and token prop
 export const socket = {
-  on: (name, callback) => {
-    return insecureSocket.on(name, callback)
-  },
-  off: name => {
-    return insecureSocket.off(name)
-  },
-  emit: (name, data = {}) => {
-    return insecureSocket.emit(name, {
-      token: localStorage.getItem('token'),
-      data
-    })
-  },
-  fetch: (name, params) => {
-    socket.emit(name, params)
-    return new Promise((resolve, reject) => {
-      socket.on(name, ({ data, error } = {}) => {
-        if (error) {
-          console.warn(name, error)
-          reject(error)
-        } else {
-          resolve(data)
-        }
-      })
-    })
-  }
+	on: (name, callback) => {
+		return insecureSocket.on(name, callback)
+	},
+	off: name => {
+		return insecureSocket.off(name)
+	},
+	emit: (name, data = {}) => {
+		return insecureSocket.emit(name, {
+			token: localStorage.getItem('token'),
+			data
+		})
+	},
+	fetch: (name, params) => {
+		socket.emit(name, params)
+		return new Promise((resolve, reject) => {
+			socket.on(name, ({ data, error } = {}) => {
+				if (error) {
+					console.warn(name, error)
+					reject(error)
+				} else {
+					resolve(data)
+				}
+			})
+		})
+	}
 }
