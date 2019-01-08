41dbc1ac3c04414cc82d418c967ee1b1bfea7334
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-09 00:12:17 +0100

    fix a bad behaviour in app component

--- client/src/components/6_layouts/app.js
+++ client/src/components/6_layouts/app.js
@@ -1,31 +1,8 @@
 import React from 'react'
 import { Providers, Router } from '../../components'
-import { socket } from '../../services'
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { key: 0 }
-  }
-
-  componentDidMount() {
-    // Re-render application on reconnect
-    socket.on('connect', () => {
-      this.setState({ key: this.state.key + 1 })
-    })
-  }
-
-  componentWillUnmount() {
-    socket.off('connect')
-  }
-
-  render() {
-    return (
-      <div key={this.state.key}>
-        <Providers>
-          <Router />
-        </Providers>
-      </div>
-    )
-  }
-}
+export const App = () => (
+  <Providers>
+    <Router />
+  </Providers>
+)
