9fe613f571e1b0f6e375f7d9855c6e298989dc1a
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-20 00:16:08 +0100

    add specific tls config

--- server/src/services/Mailer/Mailer.ts
+++ server/src/services/Mailer/Mailer.ts
@@ -9,10 +9,12 @@ export class Mailer {
         host: process.env.MAIL_HOST,
         port: Number(process.env.MAIL_PORT),
         secure: Boolean(process.env.MAIL_SECURE),
-        requireTLS: true,
         auth: {
           user: process.env.MAIL_AUTH_USER,
           pass: process.env.MAIL_AUTH_PASS
+        },
+        tls: {
+          rejectUnauthorized: false
         }
       },
       {
