f1748a2a1d0402bdfb932b6dddc9e655a18e62f5
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-19 22:46:48 +0100

    try remove requireTLS

--- server/src/services/Mailer/Mailer.ts
+++ server/src/services/Mailer/Mailer.ts
@@ -9,7 +9,6 @@ export class Mailer {
         host: process.env.MAIL_HOST,
         port: Number(process.env.MAIL_PORT),
         secure: Boolean(process.env.MAIL_SECURE),
-        requireTLS: true,
         auth: {
           user: process.env.MAIL_AUTH_USER,
           pass: process.env.MAIL_AUTH_PASS
