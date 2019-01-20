d93ef7435aed78bfb5ed93c632cd9277cc04d122
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-20 01:43:46 +0100

    enhance activation template

--- server/src/emails/activation.ts
+++ server/src/emails/activation.ts
@@ -1,4 +1,6 @@
-export const activation = (
-  activationToken: string
-) => `<p>Cliquez sur le lien ci-dessous pour activer votre compte :</p>
-<a href="https://emendare.org/activation/${activationToken}">Activer mon compte Emendare</a>`
+export const activation = (activationToken: string) => `
+<p>Cliquez sur le lien ci-dessous pour activer votre compte :</p>
+<a href="https://emendare.org/activation/${activationToken}">
+  Activer mon compte Emendare
+</a>
+`
