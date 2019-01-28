cf2657b5e4fa2e99ffe52f2367b984a89fc7ad08
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-01-28 22:25:06 +0100

    update cypress example test

--- client/cypress/integration/specs/home.spec.js
+++ client/cypress/integration/specs/home.spec.js
@@ -3,11 +3,6 @@ describe('Navigation', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('navigate to roadmap page', () => {
-    cy.contains('Roadmap').click()
-    cy.url().should('include', '/roadmap')
-  })
-
   it('navigate to code page', () => {
     cy.contains('Charte éthique').click()
     cy.url().should('include', '/charte-ethique')
