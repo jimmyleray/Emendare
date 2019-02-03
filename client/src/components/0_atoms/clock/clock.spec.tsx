4b51da481135049c4c0b15867c7ddad0fe260412
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-02-03 22:42:00 +0100

    add test on clock component

--- client/src/components/0_atoms/clock/clock.spec.tsx
+++ client/src/components/0_atoms/clock/clock.spec.tsx
@@ -5,10 +5,17 @@ import Adapter from 'enzyme-adapter-react-16'
 enzyme.configure({ adapter: new Adapter() })
 
 import { Clock } from './clock'
+import { delay } from 'lodash'
 
-it('should render a Clock', () => {
+it('should render a Clock', async () => {
   const getTime = () => new Date()
   const HOC = Clock(getTime)
   const component: any = shallow(<HOC date={new Date()} />)
   expect(component).toBeTruthy()
+  delay(() => {
+    expect(component).toBeTruthy()
+    expect(component.html()).toContain('0 seconde')
+    component.unmount()
+    expect(component).toBeFalsy()
+  }, 1000)
 })
