ff1870c22e13ff8ef35a76a3fb0ed8d7cbf4d8ef
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-02-11 22:50:57 +0100

    correct api config test

--- client/src/config/api/api.spec.ts
+++ client/src/config/api/api.spec.ts
@@ -2,7 +2,7 @@ import { apiConfig } from './api'
 
 test('config should defined some const', () => {
   expect(apiConfig.url).toBeDefined()
-  expect(apiConfig.url.test).toBeDefined()
-  expect(apiConfig.url.development).toBeDefined()
+  expect(apiConfig.url.local).toBeDefined()
+  expect(apiConfig.url.integration).toBeDefined()
   expect(apiConfig.url.production).toBeDefined()
 })
