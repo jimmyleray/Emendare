28166bc6214672a5a5d1f805e29a1e839f48d077
Matthew Atk <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-02-12 11:13:20 +0100

    Add new mail template

---
+++ server/src/emails/components/footer.ts
@@ -0,0 +1,9 @@
+/**
+ * Footer section
+ */
+export const footer = `<mj-section background-color="hsl(0, 0%, 86%)">
+    <mj-column>
+      <mj-text font-size="10px"align="center">https://emendare.org/</mj-text>
+    </mj-column>
+  </mj-section>
+  `
---
+++ server/src/emails/components/header.ts
@@ -0,0 +1,9 @@
+/**
+ * Title section
+ */
+export const header = `<mj-section background-url="https://images.pexels.com/photos/842339/pexels-photo-842339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" background-size="cover" background-repeat="no-repeat">
+    <mj-column width="500px">
+      <mj-text align="center" color="#fff" font-size="40px">Emendare</mj-text>
+    </mj-column>
+  </mj-section>
+  `
