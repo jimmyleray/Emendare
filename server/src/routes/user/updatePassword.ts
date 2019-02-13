8f931bd7aa417e13417164207e38b14efeb44eca
Matthew Atk <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-02-13 17:44:13 +0100

    add route to set the password

---
+++ server/src/routes/user/updatePassword.ts
@@ -0,0 +1,43 @@
+import socketIO from 'socket.io'
+import { User } from '../../models'
+
+// lib to hash passwords
+import bcrypt from 'bcrypt'
+
+export const updatePassword = {
+  name: 'update-password',
+  callback: ({ socket }: { socket: socketIO.Socket }) => async ({
+    data
+  }: any) => {
+    const { token, password } = data
+    if (!password || !token) {
+      socket.emit('update-password', {
+        error: {
+          code: 405,
+          message: 'Requête invalide'
+        }
+      })
+    } else {
+      const user = await User.model.findOne({ token })
+      if (!user) {
+        socket.emit('update-password', {
+          error: {
+            code: 405,
+            message: 'Token invalide'
+          }
+        })
+      } else {
+        bcrypt.hash(password, 10, async (error, hash) => {
+          if (error) console.error(error)
+          else {
+            user.password = hash
+            await user.save()
+            // send the user updated
+            socket.emit('user', { data: user })
+            socket.emit('update-password')
+          }
+        })
+      }
+    }
+  }
+}
