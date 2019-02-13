ec8732ce562b40524128a858da4aba4aefb1086f
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-02-13 10:03:43 +0100

    add alt attribute

--- server/src/emails/components/header.ts
+++ server/src/emails/components/header.ts
@@ -5,7 +5,7 @@ export const header = `
 <mj-hero vertical-align="middle" mode="fixed-height" height="110px" background-color="hsl(0, 0%, 21%)">
 <mj-text color="hsl(0, 0%, 98%)" font-size="30px" font-weight="500" align="center" vertical-align="">
   <mj-raw align="center">
-      <img style="vertical-align: middle" width="65" src="https://emendare.org/images/logo-white.png" />
+      <img alt="logo emendare" style="vertical-align: middle" width="65" src="https://emendare.org/images/logo-white.png" />
       <span>Emendare</span>
   </mj-raw>
 </mj-text>
