83071a30ea5d94e6c81d3073eb33d9346a25c66f
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-02-17 00:18:07 +0100

    correct tests with providers

--- client/src/components/1_molecules/earlyAlert/earlyAlert.spec.tsx
+++ client/src/components/1_molecules/earlyAlert/earlyAlert.spec.tsx
@@ -5,8 +5,13 @@ import Adapter from 'enzyme-adapter-react-16'
 enzyme.configure({ adapter: new Adapter() })
 
 import { EarlyAlert } from './earlyAlert'
+import { Providers } from '../../../components'
 
 it('should render a EarlyAlert', () => {
-  const component = shallow(<EarlyAlert />)
+  const component = shallow(
+    <Providers>
+      <EarlyAlert />
+    </Providers>
+  )
   expect(component).toBeTruthy()
 })
