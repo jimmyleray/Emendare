15b04503d2aff3cafd0e107900275dbcea71ec10
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-02-18 11:54:55 +0100

    less padding bottom for mobiles

--- client/src/components/6_layouts/layout/layout.tsx
+++ client/src/components/6_layouts/layout/layout.tsx
@@ -19,7 +19,7 @@ export const Layout = (props: IProps) => (
     <Navbar />
     <section className="section" style={{ flex: 1, overflowY: 'scroll' }}>
       <div className="container">
-        <main style={{ paddingBottom: '100px' }}>{props.children}</main>
+        <main style={{ paddingBottom: '50px' }}>{props.children}</main>
       </div>
     </section>
   </div>
