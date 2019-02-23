061ae684ac01dfc8ed31ec7eaefc76f535a000c2
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-02-23 14:42:02 +0100

    move userCredentials to templates

--- client/src/components/3_templates/userCredentials/userCredentials.spec.tsx
+++ client/src/components/3_templates/userCredentials/userCredentials.spec.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { userMock } from '../../../interfaces'
 
 enzyme.configure({ adapter: new Adapter() })
 
