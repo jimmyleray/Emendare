d92d87ea26ca26b72e2dd0ae606217885d10c866
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-03-01 23:07:30 +0100

    update task migration

--- server/src/tasks/databaseMigrations.ts
+++ server/src/tasks/databaseMigrations.ts
@@ -2,17 +2,17 @@ import { Text, User } from '../models'
 import { IText, IUser } from '../../../interfaces'
 
 export const databaseMigrations = async () => {
-  // Remove old rules properties and texts
+  // Remove old rules properties
   const texts: IText[] = await Text.model.find()
   texts.forEach(async text => {
     if (typeof text.rules !== 'undefined') {
       delete text.rules
       await text.save()
     }
-    if (!text.name) {
-      await text.remove()
-    }
   })
+
+  // Remove void text like old rules
+  await Text.model.remove({ name: '' })
 
   // Remove old activationToken if already activated user
   const users: IUser[] = await User.model.find()
