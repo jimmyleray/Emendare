782be7d8f7f539a0d52951636d2eaef8e040b33b
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-03-06 17:42:53 +0100

    update ux

--- client/src/components/0_atoms/box/box.spec.tsx
+++ client/src/components/0_atoms/box/box.spec.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
-import enzyme, { shallow } from 'enzyme'
 import { render } from 'react-testing-library'
-import Adapter from 'enzyme-adapter-react-16'
-
-enzyme.configure({ adapter: new Adapter() })
 
 import { Box } from './box'
 
