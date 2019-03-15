1e86fa8988610440593c4a87556a098b4c02fe60
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-03-15 16:24:59 +0100

    create useComponentSize hook

---
+++ client/src/hooks/useComponentSize/useComponentSize.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, useCallback, useRef } from 'react'
+
+export const useComponentSize = () => {
+  const ref: any = useRef()
+
+  const getSize = () => {
+    if (!ref.current) {
+      return { width: 0, height: 0 }
+    } else {
+      return {
+        width: ref.current.offsetWidth,
+        height: ref.current.offsetHeight
+      }
+    }
+  }
+
+  const [width, setWidth] = useState(getSize().width)
+  const [height, setHeight] = useState(getSize().height)
+
+  const onSizeChange = () => {
+    const { width, height } = getSize()
+    setWidth(width)
+    setHeight(height)
+  }
+
+  useEffect((): any => {
+    if (!ref.current) {
+      return
+    } else {
+      if ((width && height) === 0) {
+        const { width, height } = getSize()
+        setWidth(width)
+        setHeight(height)
+      }
+      window.addEventListener('resize', onSizeChange)
+      return () => window.removeEventListener('resize', onSizeChange)
+    }
+  }, [ref.current])
+
+  return { height, width, ref }
+}
