918fcb284128b1c11249caa4ec4d701c8efe60f7
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-03-16 17:05:32 +0100

    refacto news template

--- client/src/components/3_templates/news/news.tsx
+++ client/src/components/3_templates/news/news.tsx
@@ -6,68 +6,56 @@ import {
   Divider,
   DataContext,
   UserContext,
-  NewsList
+  NewsList,
+  I18nContext
 } from '../../../components'
 import { Socket } from '../../../services'
-import { I18nContext } from '../../5_contexts'
 
 export const News = () => {
-  const updateLastEventDate = () => {
-    Socket.emit('updateLastEventDate')
-  }
+  const { translate } = React.useContext(I18nContext)
+  const { user } = React.useContext(UserContext)
+  const { get } = React.useContext(DataContext)
 
-  return (
-    <I18nContext.Consumer>
-      {({ translate }) => (
-        <UserContext.Consumer>
-          {({ user }) => {
-            const lastEventDate = user && new Date(user.lastEventDate).getTime()
-            return (
-              <DataContext.Consumer>
-                {({ get }) => {
-                  const events = get('events')('all')
-                  if (events && events.data) {
-                    const newEventsCount = user
-                      ? events.data.filter(
-                          (event: any) =>
-                            new Date(event.created).getTime() >
-                            new Date(user.lastEventDate).getTime()
-                        ).length
-                      : 0
-                    return (
-                      <React.Fragment>
-                        <Divider
-                          content={
-                            newEventsCount > 0
-                              ? translate('NEW_EVENTS')
-                              : translate('EVENTS_LIST')
-                          }
-                        />
-                        {newEventsCount > 0 && (
-                          <Button
-                            className="is-fullwidth is-dark "
-                            style={{
-                              marginBottom: '1.5rem'
-                            }}
-                            onClick={updateLastEventDate}
-                          >
-                            {translate('MARK_AS_READ')}
-                          </Button>
-                        )}
-                        <NewsList
-                          events={events.data}
-                          lastEventDate={lastEventDate}
-                          newEventsCount={newEventsCount}
-                        />
-                      </React.Fragment>
-                    )
-                  }
-                }}
-              </DataContext.Consumer>
-            )
-          }}
-        </UserContext.Consumer>
-      )}
-    </I18nContext.Consumer>
-  )
+  const lastEventDate = user && new Date(user.lastEventDate).getTime()
+  const events = get('events')('all')
+
+  if (events && events.data) {
+    const newEventsCount = user
+      ? events.data.filter(
+          (event: any) =>
+            new Date(event.created).getTime() >
+            new Date(user.lastEventDate).getTime()
+        ).length
+      : 0
+
+    return (
+      <React.Fragment>
+        <Divider
+          content={
+            newEventsCount > 0
+              ? translate('NEW_EVENTS')
+              : translate('EVENTS_LIST')
+          }
+        />
+        {newEventsCount > 0 && (
+          <Button
+            className="is-fullwidth is-dark"
+            style={{ marginBottom: '1.5rem' }}
+            onClick={() => {
+              Socket.emit('updateLastEventDate')
+            }}
+          >
+            {translate('MARK_AS_READ')}
+          </Button>
+        )}
+        <NewsList
+          events={events.data}
+          lastEventDate={lastEventDate}
+          newEventsCount={newEventsCount}
+        />
+      </React.Fragment>
+    )
+  } else {
+    return null
+  }
 }
