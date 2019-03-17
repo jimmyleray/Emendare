f226fc5cd1e1c0480741a74c857d66f8fb27fc98
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-03-17 17:57:56 +0100

    remove useless hr in navbar

--- client/src/components/3_templates/navbar/navbar.tsx
+++ client/src/components/3_templates/navbar/navbar.tsx
@@ -17,156 +17,148 @@ export const Navbar = () => {
   const [burgerIsActive, setBurgerActive] = React.useState(false)
 
   return (
-    <React.Fragment>
-      <nav
-        className="navbar is-dark"
-        role="navigation"
-        aria-label="main navigation"
-        style={{ padding: '0 1rem' }}
-      >
-        <div className="navbar-brand">
-          <Link
-            to={path.home}
-            onClick={() => setBurgerActive(false)}
-            className={'navbar-item has-text-weight-semibold'}
-            style={{ textDecoration: 'none' }}
-          >
-            <Logo size={36} style={{ fill: 'white' }} />
-            <span style={{ marginLeft: 8 }}>Emendare</span>
-          </Link>
-          <a
-            role="button"
-            onClick={() => setBurgerActive(!burgerIsActive)}
-            className={
-              'navbar-burger burger ' + (burgerIsActive ? 'is-active' : '')
-            }
-            aria-label="menu"
-            aria-expanded={burgerIsActive}
-            data-target="navbar-menu"
-          >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </a>
-        </div>
-        <div
-          id="navbar-menu"
-          className={'navbar-menu ' + (burgerIsActive ? 'is-active' : '')}
+    <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+      style={{ padding: '0 1rem' }}
+    >
+      <div className="navbar-brand">
+        <Link
+          to={path.home}
+          onClick={() => setBurgerActive(false)}
+          className={'navbar-item has-text-weight-semibold'}
+          style={{ textDecoration: 'none' }}
         >
-          <div className="navbar-end">
-            {!userContext.isConnectionPending ? (
-              userContext.isConnected() ? (
+          <Logo size={36} style={{ fill: 'white' }} />
+          <span style={{ marginLeft: 8 }}>Emendare</span>
+        </Link>
+        <a
+          role="button"
+          onClick={() => setBurgerActive(!burgerIsActive)}
+          className={
+            'navbar-burger burger ' + (burgerIsActive ? 'is-active' : '')
+          }
+          aria-label="menu"
+          aria-expanded={burgerIsActive}
+          data-target="navbar-menu"
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+      <div
+        id="navbar-menu"
+        className={'navbar-menu ' + (burgerIsActive ? 'is-active' : '')}
+      >
+        <div className="navbar-end">
+          {!userContext.isConnectionPending ? (
+            userContext.isConnected() ? (
+              <Link
+                to={path.profile}
+                onClick={() => setBurgerActive(false)}
+                className="navbar-item"
+              >
+                {translate('MY_PROFILE')}
+              </Link>
+            ) : (
+              <React.Fragment>
                 <Link
-                  to={path.profile}
+                  to={path.login}
+                  onClick={() => setBurgerActive(false)}
+                  className="navbar-item has-text-weight-semibold"
+                >
+                  {translate('LOGIN')}
+                </Link>
+                <Link
+                  to={path.subscribe}
                   onClick={() => setBurgerActive(false)}
                   className="navbar-item"
                 >
-                  {translate('MY_PROFILE')}
+                  {translate('REGISTER')}
                 </Link>
-              ) : (
-                <React.Fragment>
-                  <Link
-                    to={path.login}
-                    onClick={() => setBurgerActive(false)}
-                    className="navbar-item has-text-weight-semibold"
-                  >
-                    {translate('LOGIN')}
-                  </Link>
-                  <Link
-                    to={path.subscribe}
-                    onClick={() => setBurgerActive(false)}
-                    className="navbar-item"
-                  >
-                    {translate('REGISTER')}
-                  </Link>
-                </React.Fragment>
-              )
-            ) : null}
+              </React.Fragment>
+            )
+          ) : null}
 
-            <DropDown className="navbar-item" isHoverable={true} navbar={true}>
-              <DropDown.Trigger title={translate('ABOUT')} />
-              <DropDown.Menu>
-                <DropDown.Item>
-                  <Link
-                    to={path.code}
-                    onClick={() => setBurgerActive(false)}
-                    className="navbar-item"
-                  >
-                    {translate('ETHIC_CODE')}
-                  </Link>
-                </DropDown.Item>
-                <DropDown.Item>
-                  <Link
-                    to={path.contributors}
-                    onClick={() => setBurgerActive(false)}
-                    className="navbar-item"
-                  >
-                    {translate('CONTRIBUTORS')}
-                  </Link>
-                </DropDown.Item>
-                <DropDown.Item>
-                  <Link
-                    to={path.legal}
-                    onClick={() => setBurgerActive(false)}
-                    className="navbar-item"
-                  >
-                    {translate('LEGAL_MENTIONS')}
-                  </Link>
-                </DropDown.Item>
-                <DropDown.Item>
-                  <Link
-                    to="https://github.com/jimmyleray/Emendare"
-                    onClick={() => setBurgerActive(false)}
-                    className="navbar-item"
-                  >
-                    {translate('SOURCES')}
-                  </Link>
-                </DropDown.Item>
-              </DropDown.Menu>
-            </DropDown>
-            <Divider
-              vertical={true}
-              className={'navbar-item is-hidden-mobile'}
-            />
-            <a
-              href="#"
-              role="button"
-              className={`navbar-item ${
-                i18nContext.actualLanguage === 'FR'
-                  ? 'has-text-weight-bold'
-                  : ''
-              }`}
-              onClick={() => {
-                i18nContext.dispatch({
-                  type: 'setLanguage',
-                  payload: 'FR'
-                })
-              }}
-            >
-              FR
-            </a>
-            <a
-              href="#"
-              role="button"
-              className={`
+          <DropDown className="navbar-item" isHoverable={true} navbar={true}>
+            <DropDown.Trigger title={translate('ABOUT')} />
+            <DropDown.Menu>
+              <DropDown.Item>
+                <Link
+                  to={path.code}
+                  onClick={() => setBurgerActive(false)}
+                  className="navbar-item"
+                >
+                  {translate('ETHIC_CODE')}
+                </Link>
+              </DropDown.Item>
+              <DropDown.Item>
+                <Link
+                  to={path.contributors}
+                  onClick={() => setBurgerActive(false)}
+                  className="navbar-item"
+                >
+                  {translate('CONTRIBUTORS')}
+                </Link>
+              </DropDown.Item>
+              <DropDown.Item>
+                <Link
+                  to={path.legal}
+                  onClick={() => setBurgerActive(false)}
+                  className="navbar-item"
+                >
+                  {translate('LEGAL_MENTIONS')}
+                </Link>
+              </DropDown.Item>
+              <DropDown.Item>
+                <Link
+                  to="https://github.com/jimmyleray/Emendare"
+                  onClick={() => setBurgerActive(false)}
+                  className="navbar-item"
+                >
+                  {translate('SOURCES')}
+                </Link>
+              </DropDown.Item>
+            </DropDown.Menu>
+          </DropDown>
+          <Divider vertical={true} className={'navbar-item is-hidden-mobile'} />
+          <a
+            href="#"
+            role="button"
+            className={`navbar-item ${
+              i18nContext.actualLanguage === 'FR' ? 'has-text-weight-bold' : ''
+            }`}
+            onClick={() => {
+              i18nContext.dispatch({
+                type: 'setLanguage',
+                payload: 'FR'
+              })
+            }}
+          >
+            FR
+          </a>
+          <a
+            href="#"
+            role="button"
+            className={`
                 navbar-item ${
                   i18nContext.actualLanguage === 'EN'
                     ? 'has-text-weight-bold'
                     : ''
                 }`}
-              onClick={() => {
-                i18nContext.dispatch({
-                  type: 'setLanguage',
-                  payload: 'EN'
-                })
-              }}
-            >
-              EN
-            </a>
-          </div>
+            onClick={() => {
+              i18nContext.dispatch({
+                type: 'setLanguage',
+                payload: 'EN'
+              })
+            }}
+          >
+            EN
+          </a>
         </div>
-      </nav>
-      <hr style={{ margin: 0 }} />
-    </React.Fragment>
+      </div>
+    </nav>
   )
 }
