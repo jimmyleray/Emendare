352f5518a139737f5720a11b39b4cba407d1861a
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-03-20 17:47:05 +0100

    add newEvents in state

--- client/src/components/5_contexts/events/events.tsx
+++ client/src/components/5_contexts/events/events.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useMemo, useReducer } from 'react'
+import React, { useEffect, useReducer, useContext } from 'react'
 import { Socket } from '../../../services'
 import { IError, IEvent } from '../../../../../interfaces'
 import _ from 'lodash'
+import { UserContext } from '../user'
+import { getNewEvent, deleteNewEvent } from './helper'
 
 interface IEventProviderProps {
   /** Children nodes */
@@ -12,17 +14,24 @@ interface IEventProviderState {
   error: any
   events: Array<IEvent>
   hasNextPage: boolean
+  newEvents: Array<IEvent>
+  dispatch: any
 }
 
 const initialState: IEventProviderState = {
   events: new Array<IEvent>(),
   hasNextPage: true,
-  error: undefined
+  error: undefined,
+  newEvents: [],
+  dispatch: () => {
+    return
+  }
 }
 
 export const EventsContext = React.createContext(initialState)
 
 export const EventsProvider = ({ children }: IEventProviderProps) => {
+  const { user } = useContext(UserContext)
   // Reducer function
   const reducer = (
     state: IEventProviderState,
@@ -31,23 +40,44 @@ export const EventsProvider = ({ children }: IEventProviderProps) => {
     switch (action.type) {
       case 'ADD_NEW_EVENTS':
         const events = [...state.events]
+        const newEvents = [...state.newEvents]
         events.unshift(action.payload.events)
+        newEvents.push(action.payload.events)
         return {
           ...state,
           error: action.payload.error,
-          events: _.uniqBy(events, '_id')
+          events: _.uniqBy(events, '_id'),
+          newEvents: _.uniqBy(newEvents, '_id')
         }
       case 'ADD_OLD_EVENTS':
+        const listEvents = _.uniqBy(
+          _.concat(state.events, action.payload.events),
+          '_id'
+        )
         return {
           ...state,
           error: action.payload.error,
-          hasNextPage: action.payload.error,
-          events: _.uniqBy(_.concat(state.events, action.payload.events), '_id')
+          hasNextPage: action.payload.hasNextPage,
+          events: listEvents,
+          newEvents: getNewEvent(action.payload.lastEventDate, listEvents)
+        }
+      case 'NEW_EVENTS_READED':
+        Socket.emit('updateLastEventDate')
+        return {
+          ...state,
+          newEvents: []
+        }
+      case 'NEW_EVENT_READED':
+        Socket.emit('updateLastEventDate')
+        return {
+          ...state,
+          newEvents: deleteNewEvent(action.payload.eventId, state.newEvents)
         }
       default:
         return state
     }
   }
+
   // Initialization of the state
   const [state, dispatch] = useReducer(reducer, initialState)
 
@@ -67,35 +97,40 @@ export const EventsProvider = ({ children }: IEventProviderProps) => {
           payload: {
             error: error,
             events: data.events,
-            hasNextPage: data.hasNextPage
+            hasNextPage: data.hasNextPage,
+            lastEventDate: user ? user.lastEventDate : null
           }
         })
       }
     )
-    Socket.on(
-      'events/new',
-      ({ error, data }: { error: IError; data: IEvent }) => {
-        dispatch({
-          type: 'ADD_NEW_EVENTS',
-          payload: {
-            error: error,
-            events: data
-          }
-        })
+    if (user) {
+      Socket.on(
+        'events/new',
+        ({ error, data }: { error: IError; data: IEvent }) => {
+          dispatch({
+            type: 'ADD_NEW_EVENTS',
+            payload: {
+              error: error,
+              events: data
+            }
+          })
+        }
+      )
+      return () => {
+        Socket.off('events')
+        Socket.off('events/new')
       }
-    )
-    return () => {
-      Socket.off('events')
-      Socket.off('events/new')
     }
-  }, [])
+  }, [user])
 
   return (
     <EventsContext.Provider
       value={{
         events: state.events,
         hasNextPage: state.hasNextPage,
-        error: state.error
+        error: state.error,
+        newEvents: state.newEvents,
+        dispatch
       }}
     >
       {children}
