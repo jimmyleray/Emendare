88629765ce4c2ed57e172b1caec87e7ccce91d00
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-03-24 16:48:50 +0100

    fix unit tests

--- client/src/components/0_atoms/icon/icon.spec.tsx
+++ client/src/components/0_atoms/icon/icon.spec.tsx
@@ -4,7 +4,7 @@ import { render } from 'react-testing-library'
 import { Icon } from './icon'
 
 it('should render an Icon', () => {
-  const { container } = render(<Icon type="solid" className="fa-user" />)
+  const { container } = render(<Icon type="solid" name="fa-user" />)
   expect(container).toBeTruthy()
   const icon: any = container.querySelector('i')
   expect(icon.classList.contains('fa-user')).toBeTruthy()
