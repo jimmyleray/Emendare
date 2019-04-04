0a6989c76dca22e3b04f8f2abde4a677aea88aa5
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-04-04 22:36:51 +0200

    asynchronous data provider

--- client/src/components/5_contexts/data/data.tsx
+++ client/src/components/5_contexts/data/data.tsx
@@ -22,30 +22,34 @@ export class DataProvider extends React.Component<{}, IDataProviderState> {
           return this.state.memo[type][id]
         } else if (!this.state.listeners.includes(listener)) {
           Socket.on(listener, ({ error, data }: any) => {
+            setTimeout(() => {
+              this.setState(
+                prevState => ({
+                  ...prevState,
+                  memo: {
+                    ...prevState.memo,
+                    [type]: { ...prevState.memo[type], [id]: { error, data } }
+                  }
+                }),
+                () => {
+                  Socket.emit('user')
+                }
+              )
+            }, 0)
+          })
+          setTimeout(() => {
             this.setState(
               prevState => ({
                 ...prevState,
-                memo: {
-                  ...prevState.memo,
-                  [type]: { ...prevState.memo[type], [id]: { error, data } }
-                }
+                listeners: [
+                  ...new Set<string>([...prevState.listeners, listener])
+                ]
               }),
               () => {
-                Socket.emit('user')
+                Socket.emit(type, { id })
               }
             )
-          })
-          this.setState(
-            prevState => ({
-              ...prevState,
-              listeners: [
-                ...new Set<string>([...prevState.listeners, listener])
-              ]
-            }),
-            () => {
-              Socket.emit(type, { id })
-            }
-          )
+          }, 0)
         }
       }
     }
