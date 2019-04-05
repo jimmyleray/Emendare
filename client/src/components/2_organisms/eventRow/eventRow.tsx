2c076d2bbe32f868720f8e08d25758d35ba0e1aa
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-05 18:06:08 +0200

    add updateRow props

--- client/src/components/2_organisms/eventRow/eventRow.tsx
+++ client/src/components/2_organisms/eventRow/eventRow.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Event, Divider, I18nContext } from '../../../components'
+import {
+  Event,
+  Divider,
+  I18nContext,
+  TextEventCard,
+  AmendEventCard,
+  ResultEventCard
+} from '../../../components'
 import { IEvent } from '../../../../../interfaces'
 
 interface IEventRowProps {
@@ -9,21 +16,47 @@ interface IEventRowProps {
   measure: () => void
   /** Tell if the event is new */
   isNew: boolean
-  /** Tell if the event is last */
-  isLast: boolean
+  /** Force a row to re-render */
+  updateRow: (index: number) => void
+  /** Index of the row */
+  index: number
 }
 
-export const EventRow = ({ data, measure, isNew, isLast }: IEventRowProps) => {
+const displayRightEvent = (
+  type: string,
+  event: IEvent,
+  updateRow: (index: number) => void,
+  index: number
+) => {
+  switch (type) {
+    case 'text':
+      return <TextEventCard event={event} />
+    case 'amend':
+      return <AmendEventCard event={event} updateRow={() => updateRow(index)} />
+    case 'result':
+      return <ResultEventCard event={event} />
+    default:
+      return <Event data={event} />
+  }
+}
+
+export const EventRow = ({
+  data,
+  measure,
+  isNew,
+  updateRow,
+  index
+}: IEventRowProps) => {
   const { translate } = React.useContext(I18nContext)
 
   return (
     <div onLoad={measure}>
-      <Event data={data} />
-      {isNew ? (
-        <Divider content={translate('OLD_EVENTS')} />
-      ) : !isLast ? (
-        <hr />
-      ) : null}
+      {data ? (
+        displayRightEvent(data.target.type, data, updateRow, index)
+      ) : (
+        <Event data={data} />
+      )}
+      {isNew ? <Divider content={translate('OLD_EVENTS')} /> : null}
     </div>
   )
 }
