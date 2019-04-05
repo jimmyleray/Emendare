c671325d4ebd941ed8846b778b973e2c1e7db3ad
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-05 18:05:25 +0200

    add withText and withResult props

--- client/src/components/1_molecules/vote/vote.tsx
+++ client/src/components/1_molecules/vote/vote.tsx
@@ -14,6 +14,10 @@ interface IVoteProps {
   className?: string
   /** Custom CSS */
   style?: React.CSSProperties
+  /** Display the text+Icon or just icon */
+  withText?: boolean
+  /** Display the result next to each button */
+  withResult?: boolean
 }
 
 const vote = (user: any) => (amend: any) => (type: string) => (
@@ -33,40 +37,66 @@ export const Vote = ({
   user,
   amend,
   match,
+  withText = true,
+  withResult = false,
   ...rest
 }: IVoteProps) => {
   const { translate } = React.useContext(I18nContext)
 
   return (
     <Buttons className={className} {...rest}>
+      {withResult && (
+        <span
+          className="has-text-success is-size-5"
+          style={{ marginRight: '0.5em' }}
+        >
+          {amend.results.upVotesCount}{' '}
+        </span>
+      )}
       <Button
-        className={`is-success is-medium ${
+        className={`is-success ${className} ${
           user.upVotes.includes(amend._id) ? '' : 'is-outlined'
         }`}
         onClick={vote(user)(amend)('up')(match.params.id)}
         disabled={amend.closed}
       >
-        <span>{translate('UP_VOTE')}</span>
+        {withText && <span>{translate('UP_VOTE')}</span>}
         <Icon type={'solid'} name="fa-smile" size="fa-lg" />
       </Button>
+      {withResult && (
+        <span
+          className="has-text-info is-size-5"
+          style={{ marginRight: '0.5em' }}
+        >
+          {amend.results.indVotesCount}{' '}
+        </span>
+      )}
       <Button
-        className={`is-info is-medium ${
+        className={`is-info ${className} ${
           user.indVotes.includes(amend._id) ? '' : 'is-outlined'
         }`}
         onClick={vote(user)(amend)('ind')(match.params.id)}
         disabled={amend.closed}
       >
-        <span>{translate('IND_VOTE')}</span>
+        {withText && <span>{translate('IND_VOTE')}</span>}
         <Icon type={'solid'} name="fa-meh" size="fa-lg" />
       </Button>
+      {withResult && (
+        <span
+          className="has-text-danger is-size-5"
+          style={{ marginRight: '0.5em' }}
+        >
+          {amend.results.downVotesCount}{' '}
+        </span>
+      )}
       <Button
-        className={`is-danger is-medium ${
+        className={`is-danger ${className} ${
           user.downVotes.includes(amend._id) ? '' : 'is-outlined'
         }`}
         onClick={vote(user)(amend)('down')(match.params.id)}
         disabled={amend.closed}
       >
-        <span>{translate('DOWN_VOTE')}</span>
+        {withText && <span>{translate('DOWN_VOTE')}</span>}
         <Icon type={'solid'} name="fa-frown" size="fa-lg" />
       </Button>
     </Buttons>
