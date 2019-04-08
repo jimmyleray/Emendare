1aedac2482ec9a578a9e87aa95f9201b85646363
Matthew <29732250+Matth10@users.noreply.github.com>
2019-04-08 16:06:02 +0200

    Update routerText.tsx

--- client/src/helpers/testUtils/routerText.tsx
+++ client/src/helpers/testUtils/routerText.tsx
@@ -4,11 +4,11 @@ import { render } from 'react-testing-library'
 import { createMemoryHistory } from 'history'
 
 export const renderWithRouter = (
-  ui: React.ReactNode,
+  children: React.ReactNode,
   { route = '/', ...renderOptions } = {}
 ) => {
   const history = createMemoryHistory({ initialEntries: [route] })
-  const utils = render(<Router history={history}>{ui}</Router>, renderOptions)
+  const utils = render(<Router history={history}>{children}</Router>, renderOptions)
   return {
     ...utils
   }
