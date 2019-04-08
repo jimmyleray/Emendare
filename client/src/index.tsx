615dcef6a0154f97b636c86dd1a471f4452546e6
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-04-08 21:39:24 +0200

    use service worker for pwa

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,4 +9,4 @@ ReactDOM.render(<Router />, document.getElementById('app-root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
