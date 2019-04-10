bff8b3070fe534d0f153293f3a0f3d505a9698f2
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-10 17:56:36 +0200

    add profilCard component

---
+++ client/src/components/3_templates/profilCard/profilCard.tsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+// Components
+import {
+  UserContext,
+  Icon,
+  I18nContext,
+  Grid,
+  Button
+} from '../../../components'
+
+export const ProfilCard = () => {
+  const userContext = useContext(UserContext)
+  const { translate } = React.useContext(I18nContext)
+
+  return (
+    userContext.user && (
+      <div className="container-column" style={{ padding: '12px 12px' }}>
+        <div className="has-text-centered">
+          <Icon name="fa-user-circle" className="fa-3x is-large" />
+          <p>{userContext.user.email}</p>
+          <Button
+            onClick={userContext.logout}
+            className="is-danger is-small"
+            style={{ marginTop: '0.5rem' }}
+          >
+            {translate('SIGN_OUT')}
+          </Button>
+          <hr />
+        </div>
+        <Grid style={{ gridTemplateColumns: '1fr 1fr' }}>
+          <Grid style={{ gridTemplateRows: '1fr 1fr' }}>
+            <div className="has-text-weight-semibold">Votes</div>
+            <div className="has-text-info has-text-weight-bold">
+              {userContext.user.downVotes.length +
+                userContext.user.upVotes.length}
+            </div>
+          </Grid>
+          <Grid style={{ gridTemplateRows: '1fr 1fr' }}>
+            <div className="has-text-weight-semibold">Textes suivis</div>
+            <div className="has-text-info has-text-weight-bold">
+              {userContext.user.followedTexts.length}
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    )
+  )
+}
