4e18f086c8fb1b94967100c001dda673eda8817c
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-10 17:57:27 +0200

    add profil card

--- client/src/components/4_pages/news/news.tsx
+++ client/src/components/4_pages/news/news.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import { News, Page, I18nContext, Grid } from '../../../components'
+import { News, Page, I18nContext, Grid, ProfilCard } from '../../../components'
 
 export const NewsPage = () => {
   const { translate } = React.useContext(I18nContext)
 
   return (
     <Page title={translate('NEWS')} style={{ padding: '0' }}>
-      <Grid style={{ gridTemplateColumns: '2fr 3.5fr 2fr' }}>
-        <div />
-        <News />
+      <Grid
+        style={{
+          gridTemplateColumns: '1fr 538px 1fr',
+          paddingTop: '0.5rem',
+          gridGap: '0.5rem'
+        }}
+      >
+        <div className="is-hidden-mobile">
+          <ProfilCard />
+        </div>
+        <div className="container-column">
+          <News />
+        </div>
         <div />
       </Grid>
     </Page>
