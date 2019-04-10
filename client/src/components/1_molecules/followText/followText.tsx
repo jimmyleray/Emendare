a688af1fbd931de7c3f9bd2f935dd294cd40545f
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-10 15:01:55 +0200

    add option with icon

--- client/src/components/1_molecules/followText/followText.tsx
+++ client/src/components/1_molecules/followText/followText.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { IText } from '../../../../../interfaces'
-import { Button } from '../../../components'
+import { Button, Icon } from '../../../components'
 import { Socket } from '../../../services'
 
 interface IFollowTextProps {
@@ -8,15 +8,38 @@ interface IFollowTextProps {
   text: IText
   /** Additional CSS UI class */
   className?: string
+  /** Display only an icon */
+  withIcon?: boolean
 }
 
-export const FollowText = ({ text, className = '' }: IFollowTextProps) => (
-  <Button
-    onClick={() => {
-      Socket.emit('followText', { id: text._id })
-    }}
-    className={`button is-success ${className}`}
-  >
-    Participer à ce texte
-  </Button>
-)
+export const FollowText = ({
+  text,
+  className = '',
+  withIcon = false
+}: IFollowTextProps) =>
+  withIcon ? (
+    <Button
+      className="has-text-grey-light"
+      style={{ border: 'none', padding: 'none' }}
+      onClick={() => {
+        Socket.emit('followText', { id: text._id })
+      }}
+    >
+      <Icon
+        type={'light'}
+        name="fa-user"
+        className="fa-lg"
+        style={{ marginRight: '0.5rem' }}
+      />
+      {text.followersCount}
+    </Button>
+  ) : (
+    <Button
+      onClick={() => {
+        Socket.emit('followText', { id: text._id })
+      }}
+      className={`button is-success ${className}`}
+    >
+      Participer à ce texte
+    </Button>
+  )
