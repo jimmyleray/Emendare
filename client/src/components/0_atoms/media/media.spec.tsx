0d2247432b41ce1b07275c7b1cd03aa96b052a97
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-15 10:38:45 +0200

    improve test

--- client/src/components/0_atoms/media/media.spec.tsx
+++ client/src/components/0_atoms/media/media.spec.tsx
@@ -4,17 +4,37 @@ import { render } from 'react-testing-library'
 import { Media } from './media'
 
 it('should render a Media', () => {
-  const { container, getByText } = render(
-    <Media>
-      Media
-      <Media.Left>Left</Media.Left>
-      <Media.Content>Content</Media.Content>
-      <Media.Right>Right</Media.Right>
-    </Media>
+  const { container, getByText } = render(<Media className="test">Media</Media>)
+  expect(container).toBeTruthy()
+  const mediaNode: any = container.querySelector('div')
+  expect(mediaNode.classList.contains('media')).toBeTruthy()
+  expect(mediaNode.classList.contains('test')).toBeTruthy()
+})
+
+it('should render a Media Left', () => {
+  const { container } = render(<Media.Left className="test">Media</Media.Left>)
+  expect(container).toBeTruthy()
+  const mediaNode: any = container.querySelector('div')
+  expect(mediaNode.classList.contains('media-left')).toBeTruthy()
+  expect(mediaNode.classList.contains('test')).toBeTruthy()
+})
+
+it('should render a Media Content', () => {
+  const { container } = render(
+    <Media.Content className="test">Media</Media.Content>
   )
   expect(container).toBeTruthy()
-  expect(getByText('Media')).toBeTruthy()
-  expect(getByText('Left')).toBeTruthy()
-  expect(getByText('Right')).toBeTruthy()
-  expect(getByText('Content')).toBeTruthy()
+  const mediaNode: any = container.querySelector('div')
+  expect(mediaNode.classList.contains('media-content')).toBeTruthy()
+  expect(mediaNode.classList.contains('test')).toBeTruthy()
+})
+
+it('should render a Media Right', () => {
+  const { container } = render(
+    <Media.Right className="test">Media</Media.Right>
+  )
+  expect(container).toBeTruthy()
+  const mediaNode: any = container.querySelector('div')
+  expect(mediaNode.classList.contains('media-right')).toBeTruthy()
+  expect(mediaNode.classList.contains('test')).toBeTruthy()
 })
