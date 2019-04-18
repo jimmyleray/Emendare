1f00b0bec033448ecfcb139d502bd9adfe6fb4ee
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-18 17:38:14 +0200

    add provider

---
+++ server-nest/src/providers/database.provider.ts
@@ -0,0 +1,20 @@
+import { createConnection, ConnectionOptions } from 'typeorm'
+
+const databaseConfig: ConnectionOptions = {
+  type: 'mongodb',
+  host: process.env.MONGODB_ADDON_HOST || 'localhost',
+  port: Number(process.env.MONGODB_ADDON_PORT) || 27017,
+  username: process.env.MONGODB_ADDON_USER || '',
+  password: process.env.MONGODB_ADDON_PASSWORD || '',
+  database: process.env.MONGODB_ADDON_DB || 'register',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+  useNewUrlParser: true
+}
+
+export const databaseProvider = [
+  {
+    provide: 'DbConnectionToken',
+    useFactory: async () => await createConnection(databaseConfig)
+  }
+]
