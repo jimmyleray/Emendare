f7375e781015502820888e64536f5a367e4db815
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-18 17:37:27 +0200

    add entity

---
+++ server-nest/src/entities/text.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, ObjectID, ObjectIdColumn, BaseEntity } from 'typeorm'
+import { Amend } from '../entities'
+
+@Entity()
+export class Text {
+  @ObjectIdColumn()
+  id: ObjectID
+
+  @Column({ default: Date.now })
+  created: Date
+
+  @Column()
+  name: string
+
+  @Column()
+  description: string
+
+  @Column({ default: 0 })
+  followersCount: number
+
+  @Column({ default: '' })
+  actual: string
+
+  @Column({ default: [] })
+  patches: string[]
+
+  @Column(type => Amend)
+  amends: Array<Amend['id']>
+
+  constructor(name: string, description: string) {
+    this.name = name
+    this.description = description
+  }
+}
