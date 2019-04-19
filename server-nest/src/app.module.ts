6f94486e51bc2fb5b0d61d45fc4fbbc36c37d145
Matth10 <matthew.atkinson@etudiant.univ-rennes1.fr>
2019-04-19 17:19:13 +0200

    add entities

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common'
 import { databaseProvider } from './providers'
+import { TypeOrmModule } from '@nestjs/typeorm'
+// Gateway
 import {
   EventGateway,
   AmendGateway,
   UserGateway,
   TextGateway
 } from './gateways'
+// Entities
+import { User, Amend, Text, Event } from './entities'
 
+// services
 import {
   EventService,
   UserService,
@@ -15,8 +20,18 @@ import {
 } from './services'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Amend]),
+    TypeOrmModule.forFeature([Text]),
+    TypeOrmModule.forFeature([Event])
+  ],
   providers: [
     ...databaseProvider,
+    EventService,
+    UserService,
+    TextService,
+    AmendService,
     EventGateway,
     AmendGateway,
     UserGateway,
