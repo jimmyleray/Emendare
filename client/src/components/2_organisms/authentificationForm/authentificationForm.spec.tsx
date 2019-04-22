7bf481d2ddfcb49e74ed36c9b524193313e8c125
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-04-23 00:45:01 +0200

    fix tests

--- client/src/components/2_organisms/authentificationForm/authentificationForm.spec.tsx
+++ client/src/components/2_organisms/authentificationForm/authentificationForm.spec.tsx
@@ -17,23 +17,12 @@ it('should render a AuthentificationForm and login form', async () => {
 })
 
 it('should render a AuthentificationForm pass to login/register and register/login', async () => {
-  const { container, getByText, getByPlaceholderText } = renderWithRouter(
+  const { container, getByPlaceholderText } = renderWithRouter(
     <I18nProvider>
       <AuthentificationForm />
     </I18nProvider>
   )
   expect(container).toBeTruthy()
-
-  fireEvent.click(getByText("Je n'ai pas de compte"))
-
-  expect(await getByPlaceholderText('Email')).toBeTruthy()
-  expect(await getByPlaceholderText('Nouveau mot de passe')).toBeTruthy()
-  expect(
-    await getByPlaceholderText('Verification du mot de passe')
-  ).toBeTruthy()
-
-  fireEvent.click(getByText("J'ai déjà un compte"))
-
   expect(await getByPlaceholderText('Email')).toBeTruthy()
   expect(await getByPlaceholderText('Mot de passe')).toBeTruthy()
 })
--- client/src/services/title/title.spec.ts
+++ client/src/services/title/title.spec.ts
@@ -3,13 +3,13 @@ import { Title } from './title'
 test('Title has some utils methods', () => {
   expect(Title.documentTitle).toBe('Emendare')
   Title.pageTitle = 'Test'
-  expect(Title.documentTitle).toBe('Emendare | Test')
+  expect(Title.documentTitle).toBe('Emendare / Test')
   Title.badgeCount = 2
-  expect(Title.documentTitle).toBe('(2) Emendare | Test')
+  expect(Title.documentTitle).toBe('(2) Emendare / Test')
   Title.pageTitle = 'Accueil'
-  expect(Title.documentTitle).toBe('(2) Emendare | Accueil')
+  expect(Title.documentTitle).toBe('(2) Emendare / Accueil')
   Title.badgeCount = 0
-  expect(Title.documentTitle).toBe('Emendare | Accueil')
+  expect(Title.documentTitle).toBe('Emendare / Accueil')
   Title.pageTitle = ''
   expect(Title.documentTitle).toBe('Emendare')
 })
