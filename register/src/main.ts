7c6315ad17a0f5dfe4ff7dc5d016d85a46f08d8d
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-06-09 00:44:23 +0200

    :arrow_up: upgrade register dependencies

--- register/src/main.ts
+++ register/src/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as rateLimit from 'express-rate-limit'
 import * as compression from 'compression'
 import * as bodyParser from 'body-parser'
 import * as helmet from 'helmet'
 
 NestFactory.create(AppModule).then(app => {
-  // Rate Limit
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100 // limit each IP to 100 requests per windowMs
-    })
-  )
 
   // Security
   app.use(helmet())
