9ee517e82d240da763e5a9c0e267534b67df3e52
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-06-10 01:30:12 +0200

    :white_check_mark: fix register tests

--- register/src/controllers/register/register.controller.ts
+++ register/src/controllers/register/register.controller.ts
@@ -1,18 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Req,
-  UseInterceptors,
-  CacheInterceptor
-} from '@nestjs/common'
+import { Controller, Get, Post, Req } from '@nestjs/common'
 import { Request } from 'express'
 import { Register } from '../../entities'
 
 @Controller()
 export class RegisterController {
   @Get()
-  @UseInterceptors(CacheInterceptor)
   async findAll() {
     const registers = await Register.find()
     return registers.filter(register => !!register.url && register.url !== '/')
