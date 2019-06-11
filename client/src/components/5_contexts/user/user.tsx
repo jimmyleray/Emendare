33330c3004ec1786d1fc78b540eaed4b6258f3ab
Matthew <matthew.atkinson@protonmail.com>
2019-06-11 16:59:39 +0200

    :art: improve user provider

--- client/src/components/5_contexts/user/user.tsx
+++ client/src/components/5_contexts/user/user.tsx
@@ -1,60 +1,106 @@
 import React from 'react'
 import { ApiContext } from '../../../components'
 import { IError, IUser } from '../../../../../interfaces'
+import {
+  initialState,
+  setErrorAuthAction,
+  setIsConnectionPendingAction,
+  setUserAction,
+  userReducer
+} from './store'
 
-interface IUserProviderState {
+interface IUserProviderValue {
   user: IUser | null
   isConnectionPending: boolean
   isConnected: any
   logout: any
+  register: any
+  errorAuth: IError | null
+  login: any
 }
 
-export const UserContext = React.createContext({} as IUserProviderState)
+const UserContext = React.createContext({} as IUserProviderValue)
 
 export const UserProvider = ({ children }: any) => {
   const { Socket } = React.useContext(ApiContext)
-  const [user, setUser] = React.useState<IUser | null>(null)
-  const [isConnectionPending, setIsConnectionPending] = React.useState(true)
 
-  const isConnected = () => user !== null
+  const [state, dispatch] = React.useReducer(userReducer, initialState)
+
+  const isConnected = () => state.user !== null
   const logout = () => {
     localStorage.removeItem('token')
-    setUser(null)
+    dispatch(setUserAction(null))
+  }
+  const register = (email: string, password: string) => {
+    Socket.fetch('subscribe', { email, password })
+      .then(() => {
+        dispatch(setErrorAuthAction(null))
+        return true
+      })
+      .catch((err: any) => {
+        dispatch(setErrorAuthAction(err))
+        return false
+      })
+  }
+  const login = (email?: string, password?: string) => {
+    Socket.fetch('login', { email, password })
+      .then(({ user, token }: any) => {
+        dispatch(setUserAction(user))
+        localStorage.setItem('token', token)
+        dispatch(setIsConnectionPendingAction(false))
+        dispatch(setErrorAuthAction(null))
+        return true
+      })
+      .catch((err: any) => {
+        localStorage.removeItem('token')
+        dispatch(setIsConnectionPendingAction(false))
+        dispatch(setErrorAuthAction(err))
+        return false
+      })
   }
 
   React.useEffect(() => {
     Socket.on('user', ({ error, data }: { error: IError; data: IUser }) => {
       if (!error) {
-        setUser(data)
+        dispatch(setUserAction(data))
       }
     })
 
     const token = localStorage.getItem('token')
     if (token) {
-      Socket.fetch('login')
-        .then(({ user }: any) => {
-          setUser(user)
-          setIsConnectionPending(false)
-        })
-        .catch(() => {
-          localStorage.removeItem('token')
-          setIsConnectionPending(false)
-        })
+      login()
     } else {
-      setIsConnectionPending(false)
+      dispatch(setIsConnectionPendingAction(false))
     }
 
     return () => {
       Socket.off('user')
       Socket.off('login')
+      Socket.off('subscribe')
     }
   }, [])
 
   return (
     <UserContext.Provider
-      value={{ user, isConnected, isConnectionPending, logout }}
+      value={{
+        ...state,
+        logout,
+        register,
+        isConnected,
+        login
+      }}
     >
       {children}
     </UserContext.Provider>
   )
+}
+
+export const useUser = () => {
+  const context = React.useContext(UserContext)
+
+  if (!context) {
+    throw new Error('useUser must be used inside the UserProvider')
+  }
+
+  return context
 }
