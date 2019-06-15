414639dfb6c78450d5725d7c929a0fdbf350593f
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-06-15 10:22:04 +0200

    :white_check_mark: fix client unit tests

--- client/src/services/amend/amend.spec.ts
+++ client/src/services/amend/amend.spec.ts
@@ -3,9 +3,9 @@ import { Amend } from './amend'
 test('Amend is voted by a user method', () => {
   const user = { upVotes: ['test1'], downVotes: [], indVotes: ['test3'] }
 
-  const amend1 = { _id: 'test1' }
-  const amend2 = { _id: 'test2' }
-  const amend3 = { _id: 'test3' }
+  const amend1 = { id: 'test1' }
+  const amend2 = { id: 'test2' }
+  const amend3 = { id: 'test3' }
 
   expect(Amend.isVoted(user)(amend1)).toBe(true)
   expect(Amend.isVoted(user)(amend2)).toBe(false)
