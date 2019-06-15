7262bc1687f828cbbb1686230cd577b00ccd5129
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-06-15 12:14:07 +0200

    :recycle: prefer use async/await

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 import * as helmet from 'helmet'
 import * as compression from 'compression'
 import * as bodyParser from 'body-parser'
+
 // Chalk for colored logs
 import chalk from 'chalk'
 
@@ -10,22 +11,22 @@ async function bootstrap() {
   // Initial server log
   console.log(chalk.green('> Emendare Server launch\n'))
 
-  NestFactory.create(AppModule).then(app => {
-    // Security
-    app.use(helmet())
-    app.enableCors()
+  const app = await NestFactory.create(AppModule)
+  // Security
+  app.use(helmet())
+  app.enableCors()
 
-    // Body Parsing
-    // app.use(bodyParser.json()) // for parsing json
-    // app.use(bodyParser.urlencoded({ extended: true })) // for parsing x-www-form-urlencoded
+  // Body Parsing
+  app.use(bodyParser.json()) // for parsing json
+  app.use(bodyParser.urlencoded({ extended: true })) // for parsing x-www-form-urlencoded
 
-    // Compression
-    app.use(compression())
+  // Compression
+  app.use(compression())
 
-    // Start and listening on a specific port
-    const port = Number(process.env.PORT) || 3030
-    app.listen(port)
-    console.log(chalk.green(`Emendare server listening on port ${port}`))
-  })
+  // Start and listening on a specific port
+  const port = Number(process.env.PORT) || 3030
+  app.listen(port)
+  console.log(chalk.green(`Emendare server listening on port ${port}`))
 }
+
 bootstrap()
