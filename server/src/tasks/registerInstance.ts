e58e88e5991bb325f2a248e7b03855c9c782cc77
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-06-26 18:05:16 +0200

    :lipstick: enhance registerInstance task

--- server/src/tasks/registerInstance.ts
+++ server/src/tasks/registerInstance.ts
@@ -1,20 +1,24 @@
 import fetch from 'node-fetch'
 import config from '../config'
-
-// Chalk for colored logs
 import chalk from 'chalk'
 
 export const registerInstance = async () => {
-  await fetch(config.registerUrl + 'registerInstance', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      instanceUrl: config.instance.instanceUrl
+  if (config.instance.instanceUrl) {
+    await fetch(config.registerUrl + 'registerInstance', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        instanceUrl: config.instance.instanceUrl
+      })
     })
-  })
 
-  console.log(chalk.green('Call for register instance done\n'))
+    console.log(chalk.cyan('Call for register instance done\n'))
+  } else {
+    console.log(
+      chalk.redBright('Need to set INSTANCE_URL environment variable !\n')
+    )
+  }
 }
