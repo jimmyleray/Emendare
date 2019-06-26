ed25d8b873e12b65701a5f5b8dbdebd443b264a1
Jimmy LERAY <jimmy.leray@protonmail.com>
2019-06-26 20:26:27 +0200

    :truck: move electron app in client directory

--- client/main.js
+++ client/main.js
@@ -9,22 +9,20 @@ let mainWindow
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    },
-    icon: path.join(__dirname, 'assets/icons/logo.ico')
+    width: 1400,
+    height: 900,
+    title: 'Emendare',
+    autoHideMenuBar: true
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('./build/index.html')
+  mainWindow.loadURL('http://localhost:3000')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -38,20 +36,16 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', function() {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+  if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
---
+++ electron/build/precache-manifest.20fc69899a71e3a54ddeb9afef36d43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "47b22346911acbc6a52c",
+    "url": "/static/js/main.15ffa237.chunk.js"
+  },
+  {
+    "revision": "51fe35f20b711305968f",
+    "url": "/static/js/2.82635fbd.chunk.js"
+  },
+  {
+    "revision": "47b22346911acbc6a52c",
+    "url": "/static/css/main.ae12e4d2.chunk.css"
+  },
+  {
+    "revision": "51fe35f20b711305968f",
+    "url": "/static/css/2.b7df4637.chunk.css"
+  },
+  {
+    "revision": "f2e725ede8672cf01907db9358a072e0",
+    "url": "/index.html"
+  }
+];
