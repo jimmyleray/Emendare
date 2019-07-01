103f67808852e823557740b7250e53dbeea0d004
Matthew <matthew.atkinson@protonmail.com>
2019-07-01 11:33:32 +0200

    enhance query get events

--- client/src/graphql/queries/queries.ts
+++ client/src/graphql/queries/queries.ts
@@ -1,9 +1,19 @@
 import { gql } from 'apollo-boost'
 
 export const GET_EVENTS = gql`
-  query {
-    events {
-      data
+  query($limit: Int, $lastEventDate: String) {
+    events(limit: $limit, lastEventDate: $lastEventDate) {
+      data {
+        events {
+          id
+          created
+          target {
+            type
+            id
+          }
+        }
+        hasNextPage
+      }
       error {
         message
         code
