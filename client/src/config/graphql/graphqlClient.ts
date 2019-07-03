037c178c8591b0975c6c6b16627d52733a689b6f
Matthew <matthew.atkinson@protonmail.com>
2019-07-03 14:40:53 +0200

    :bug: fix merge problem

---
+++ client/src/config/graphql/graphqlClient.ts
@@ -0,0 +1,38 @@
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+
+// Create an httpLink
+export const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API_URL || 'http://localhost:3030/graphql'
+})
+
+// Create an websocket link
+export const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5000/',
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+// Create the client
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link
+})
--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,2 +1,3 @@
 export * from './routes'
 export * from './i18n'
+export * from './graphql'
